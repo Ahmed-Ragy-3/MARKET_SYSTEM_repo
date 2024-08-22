744e027c26eb76edd87c8f6df648ab62a3edd609
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-08-22 14:46:21 +0300

    system Design to be updated

--- Database_info.md
+++ Database_info.md
@@ -7,5 +7,31 @@ Database info
    Service Name: ORCL
    Connect using Oracle client: OraDB21Home1
 
-Tables
-------> still unknown
+
+Tables:
+Products
+   ID (hidden except for admin) (PK)
+   Name
+   Price
+   Rate
+   Discount (if exists)
+   Images
+   Category ID (FK)
+   Company responsible for the product
+   Available number of product (hidden except for admin and if <= 10 products available)
+   Description or specifications or Highlights 
+
+Category
+   ID
+   NAME
+   Number of Items
+
+Users
+   ID
+   Username
+   Password (encrypted or hashed)
+
+Stores
+   Item_ID
+   quantity
+   Arrival_Date
--- Item_GUI.java
+++ Item_GUI.java
@@ -1,24 +1,24 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.layout.VBox;
+// import javafx.stage.Stage;
 
-public class Item_GUI extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button("Say 'Hello World'");
-        btn.setOnAction(e -> System.out.println("Hello World"));
+// public class Item_GUI extends Application {
+//     @Override
+//     public void start(Stage primaryStage) {
+//         Button btn = new Button("Say 'Hello World'");
+//         btn.setOnAction(e -> System.out.println("Hello World"));
 
-        VBox root = new VBox(btn);
-        Scene scene = new Scene(root, 300, 250);
+//         VBox root = new VBox(btn);
+//         Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+//         primaryStage.setTitle("Hello World!");
+//         primaryStage.setScene(scene);
+//         primaryStage.show();
+//     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//     public static void main(String[] args) {
+//         launch(args);
+//     }
+// }
