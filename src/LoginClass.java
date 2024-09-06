9f22e7e5b37ad6f82662671c47937e49c62c0330
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2024-09-06 20:10:07 +0300

    ...

---
+++ src/LoginClass.java
@@ -0,0 +1,13 @@
+import javafx.event.ActionEvent;
+
+public class LoginClass {
+   public boolean isAdmin;
+   
+   public void back_button(ActionEvent event) {
+      Runner.display("Home");
+   }
+
+   public static void main(String[] args) {
+      
+   }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,53 +1,39 @@
-//import VIP.javafx-sdk-22.0.2.javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 import javafx.event.ActionEvent;
 
 public class Runner extends Application {
    public static boolean registered;
+   public static boolean isAdmin;
    public static final String SHOPNAME = "OnShopping";
 
    public static Stage stage;
-   public static Scene scene;
    public static Parent root;
 
    public void login_button(ActionEvent event) {
-      try {
-         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-         display("Login");
-      } catch (Exception e) {
-         System.out.println("print Stack trace 1");
-         e.printStackTrace();
-      }
+      display("Login");
    }
 
-   // Static method to display a specific screen
    public static void display(String screen) {
       try {
          root = FXMLLoader.load(Runner.class.getResource(screen + ".fxml"));
-         scene = new Scene(root);
+         stage.setScene(new Scene(root));
          stage.setTitle(SHOPNAME + " - " + screen);
-         stage.setScene(scene);
          stage.setFullScreen(true);
          stage.show();
       } catch (Exception e) {
-         System.out.println("print Stack trace 2");
+         System.out.println("Error when displaying " + screen);
          e.printStackTrace();
       }
    }
 
    @Override
    public void start(Stage primaryStage) throws Exception {
-      try {
-         stage = primaryStage;
-         display("Home"); // Display the initial "Home" screen
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+      stage = primaryStage;
+      display("Home");  // Starts with the Login screen
    }
 
    public static void main(String[] args) {
